0f66be6024de984507ababca5836d65bca48faed
Mimi Netto <michnetto249@gmail.com>
2020-10-06 15:38:49 -0400

    troubleshooting auth

--- public/login.jsx
+++ public/login.jsx
@@ -2,20 +2,16 @@ class NameForm extends React.Component {
     constructor(props) {
       super(props);
       this.state = {value: ''};
-  
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
-  
     handleChange(event) {
       this.setState({value: event.target.value});
     }
-  
     handleSubmit(event) {
       alert('A login was submitted: ' + this.state.value);
       event.preventDefault();
     }
-  
     render() {
       return (
         <form onSubmit={this.handleSubmit}>
