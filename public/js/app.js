16785e65d362ab31cfe0896bf5cdc7349dd4bac0
Mimi Netto <michnetto249@gmail.com>
2020-10-05 20:39:14 -0400

    added modals

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@ class App extends React.Component {
   post: '',
   mood: '',
   img:'',
+  createdAt:'',
   blogs:[]
 }
 componentDidMount = () => {
@@ -41,39 +42,35 @@ deleteBlog = event => {
     })
   })
 }
+updateBlogs = (newBlogs) => {
+  this.setState({
+    blogs: newBlogs
+  })
+}
 updateBlog = (event) => {
   event.preventDefault()
   const id = event.target.id
   axios
-    .put('/blogs/' + id, this.state)
+    .put('/blogs/' + id, {
+      title: this.state.title,
+      post: this.state.post,
+      mood: this.state.mood,
+      img: this.state.img
+    })
     .then(response => {
       this.setState({
-        blogs: response.data,
         title: '',
+        post: '',
+        mood: '',
+        img:'',
+        blogs: response.data
       })
     })
 }
   render = () => {
     return (
       <div>
-        <details>
-          <summary>Add Blog</summary>
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="title">Title: </label>
-            <input type="text" id="title" onChange={this.handleChange}/>
-            <br />
-            <label htmlFor="post">Post: </label>
-            <input type="text" id="post" onChange={this.handleChange}/>
-            <br />
-            <label htmlFor="mood">Mood: </label>
-            <input type="text" id="mood" onChange={this.handleChange}/>
-            <br />
-            <label htmlFor="img">Image: </label>
-            <input type="text" id="img" onChange={this.handleChange}/>
-            <br />
-            <input type="submit" value="Create Blog" />
-          </form>
-          </details>
+          <Modal updateBlogs={this.updateBlogs} />
           <h2 className="recent">Most Recent Posts Blogs</h2>
           <ul>
             {this.state.blogs.map(blog => { return (
@@ -84,6 +81,7 @@ updateBlog = (event) => {
                   <summary>More info</summary>
                   <p className="view">mood: {blog.mood}</p>
                   <p className="view">post: {blog.post}</p>
+                  <p className="view">Posted: {(new Date(blog.createdAt)).toLocaleString()}</p>
                   <details>
                   <summary>Edit info</summary>
                   <form id= {blog._id} onSubmit= {this.updateBlog}>
@@ -129,6 +127,75 @@ updateBlog = (event) => {
           </ul>
         </div>
     )
+  }
+}
+const display = {
+  display: 'block'
+};
+const hide = {
+  display: 'none'
+};
+
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      toggle: false
+    }
+  }
+  handleChange = event => {
+    this.setState({ [event.target.id]: event.target.value})
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+    event.target.reset()
+    axios
+    .post('/blogs', this.state)
+    .then(response =>
+      this.props.updateBlogs(response.data)
+    )
+  }
+  toggle(event) {
+    this.setState((prevState) => ({
+      toggle: !prevState.toggle
+    }));
+  }
+  render = () => {
+    const modalContent = (
+      <div className="modal" style={this.state.toggle ? display : hide}>
+      <div className="modal-content">
+      <div className="modal-header">
+        <a className="btn-flat right" onClick={this.toggle}>X Cancel</a>
+      </div>
+        <h4 className="vibes">Post some good vibes!</h4>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="title">Title</label>
+          <input placeholder="Post Title" type="text" id="title" onChange={this.handleChange}/>
+          <br />
+          <div className="input-field">
+          <i class="material-icons prefix">create</i>
+          <label htmlFor="post">Your Post </label>
+          <textarea type="text" id="post" onChange={this.handleChange} className="materialize-textarea"></textarea>
+          </div>
+          <br />
+          <label htmlFor="mood">Mood: </label>
+          <input placeholder="What's your mood" type="text" id="mood" onChange={this.handleChange}/>
+          <br />
+          <label htmlFor="img">Image: </label>
+          <input placeholder="http://" type="text" id="img" onChange={this.handleChange}/>
+          <br />
+          <input type="submit" onClick={this.toggle} value="Post!" />
+        </form>
+      </div>
+    </div>
+    );
+    return (
+      <div>
+        <a className="waves-effect waves-light btn vibes-btn" onClick={this.toggle}>Post!</a>
+        {modalContent}
+      </div>
+    );
   }
 }
 
